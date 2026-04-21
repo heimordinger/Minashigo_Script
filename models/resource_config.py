efa5eedf1238da1b5efbcc19a5465a2af70eaf82
heimordinger <1980591890@qq.com>
2026-04-21 22:46:21 +0800

    fix model download urls

--- models/resource_config.py
+++ models/resource_config.py
@@ -3,26 +3,26 @@
 MODELS = [
     {
         "name": "det_infer",
-        "type": "dir",
+        "type": "zip",
         "path": "models/det_infer",
-        "url": "https://github.com/heimordinger/Minashigo_Script/releases/download/v1.0/det_infer.zip"
+        "url": "https://github.com/heimordinger/Minashigo_Script/releases/download/v1.0.0/det_infer.zip"
     },
     {
         "name": "rec_infer",
-        "type": "dir",
+        "type": "zip",
         "path": "models/rec_infer",
-        "url": "https://github.com/heimordinger/Minashigo_Script/releases/download/v1.0/rec_infer.zip"
+        "url": "https://github.com/heimordinger/Minashigo_Script/releases/download/v1.0.0/rec_infer.zip"
     },
     {
         "name": "cls_infer",
-        "type": "dir",
+        "type": "zip",
         "path": "models/cls_infer",
-        "url": "https://github.com/heimordinger/Minashigo_Script/releases/download/v1.0/cls_infer.zip"
+        "url": "https://github.com/heimordinger/Minashigo_Script/releases/download/v1.0.0/cls_infer.zip"
     },
     {
         "name": "tesseract",
-        "type": "dir",
+        "type": "zip",
         "path": "models/Tesseract_OCR",
-        "url": "https://github.com/heimordinger/Minashigo_Script/releases/download/v1.0/tesseract.zip"
+        "url": "https://github.com/heimordinger/Minashigo_Script/releases/download/v1.0.0/Tesseract_OCR.zip"
     }
 ]
